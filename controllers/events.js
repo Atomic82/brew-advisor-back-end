fbbee7510f88e2ead050b26b4f5cb87646cb206c
Jana Pascual <jacpascual@gmail.com>
2022-03-25 14:58:24 -0400

    Adds app models and schema and adds controller files for events, breweries, and reviews

--- controllers/events.js
+++ controllers/events.js
@@ -1,19 +1,19 @@
 import { Event } from '../models/event.js';
 import { Review } from '../models/review.js'
 
-function create(req, res) {
-  console.log("Create Event")
-}
-function show(req, res) {
-  console.log("Show event details")
-}
-function update(req, res) {
-  console.log("Update this event")
-}
+// function create(req, res) {
+//   console.log("Create Event")
+// }
+// function show(req, res) {
+//   console.log("Show event details")
+// }
+// function update(req, res) {
+//   console.log("Update this event")
+// }
 
-function deleteEvent(req, res) {
-  console.log("Delete this event")
-}
+// function deleteEvent(req, res) {
+//   console.log("Delete this event")
+// }
 
 // Delete
 // Create
@@ -21,8 +21,8 @@ function deleteEvent(req, res) {
 // Show
 
 export {
-  create,
-  show,
-  update,
-  deleteEvent as delete
+  // create,
+  // show,
+  // update,
+  // deleteEvent as delete
 }
