721dc2520d74dc7cca970ef706ff8b56a70ee021
Jana Pascual <jacpascual@gmail.com>
2022-03-28 17:03:38 -0400

    Adds functionality to review create function to push reviews array into profile and brewery models

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,4 +1,6 @@
 import { Review } from '../models/review.js'
+import { Profile } from '../models/profile.js'
+import { Brewery } from '../models/brewery.js'
 
 function index(req, res) {
   console.log("See these reviews")
@@ -15,12 +17,45 @@ function index(req, res) {
 
 function create (req, res) {
   req.body.owner = req.user.profile
+  req.body.brewery = req.brewery.name
   Review.create(req.body)
   console.log(req.body)
   .then(review => {
     review.populate("owner", "brewery")
     .then(populatedReview => {
       res.status(201).json(populatedReview)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
+  Brewery.findyById(req.brewery.id)
+  .then(brewery => {
+    brewery.reviews.push(req.body)
+    brewery.save()
+    .then(updatedBrewery => {
+      res.json(updatedBrewery)
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json(err)
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(500).json(err)
+  })
+  Profile.findById(req.user.profile)
+  .then(profile => {
+    profile.reviews.push(req.body)
+    profile.save()
+    .then(updatedProfile => {
+      res.json(updatedProfile)
     })
     .catch(err => {
       console.log(err)
